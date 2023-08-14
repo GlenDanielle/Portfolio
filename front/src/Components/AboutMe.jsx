62543d05181f74bf98a16e8ff81b74fd0d02e8c1
Glen Danielle D. Galit <glendanielleg@gmail.com>
2023-08-14 15:57:53 +0800

    pagod na

--- front/src/Components/AboutMe.jsx
+++ front/src/Components/AboutMe.jsx
@@ -14,14 +14,36 @@ const AboutMe = ({data}) => {
 
           <div className='container'>
             <div className='container1'>
-              <div className='content1'>
+
+              <div className='content1-1'>
+
                 <h1>Hello! I'm Glen Danielle Delavega Galit</h1>
-                <h2 className='intro'>I'm currently a 4th year college student, a beginner at webdev who wants to be a full stack developer</h2>
+                <h2 className='intro'>I'm a 4th year college student, a beginner at webdev who wants to be a full stack developer</h2>
+              
               </div>
-              <div className='content2'>
+
+              <div className='content1-2'>
+
                 <img src={data.me.img}></img>
+
               </div>
+
             </div>
+
+            <div className='container2'>
+
+              <div className='container2-1'>
+
+              </div>
+              <div className='container2-2'>
+                <div className='imgContainer2-1'>
+                  <img src='https://i0.wp.com/programmingwithmosh.com/wp-content/uploads/2019/01/2000px-React-icon.svg_.png?fit=2000%2C1413&ssl=1'></img>
+                
+                  </div>
+              </div>
+
+            </div>
+
           </div>
 
         </div>
