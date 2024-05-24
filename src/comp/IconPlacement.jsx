c034e41abdd1c1a7429494be1dfbf3ef4478c749
Glen Danielle D. Galit <glendanielleg@gmail.com>
2024-05-24 21:13:11 +0800

    fixed light mode and dark mode

--- src/comp/IconPlacement.jsx
+++ src/comp/IconPlacement.jsx
@@ -7,7 +7,7 @@ const IconPlacement = ({IconData}) => {
     const [showTooltip, setShowTooltip] = useState(null)
     const Icons = ()=>{
         return IconData.map((el, index)=>(
-            <div key={index} className='IconContainer' onMouseEnter={()=>{setShowTooltip(index)}} onMouseLeave={()=>{setShowTooltip(null)}}>
+            <div key={index} className={el.name == 'JRU' || el.name == "RTU" ? 'EducContainer' : 'IconContainer'} onMouseEnter={()=>{setShowTooltip(index)}} onMouseLeave={()=>{setShowTooltip(null)}}>
                 {showTooltip === index && el.name != 'JRU' && el.name != "RTU" ? <p className='Tooltip'>{el.name}</p> : null}
                 <img className='Image' src={el.img} alt={el.name} onClick={()=>{el.link ? GoToLink(el.link) : null}}/>
             </div>
