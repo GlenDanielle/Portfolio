372b075eae71717a505502690ddaad7d312c43fb
Glen Danielle D. Galit <glendanielleg@gmail.com>
2024-05-24 21:53:08 +0800

    fixed the resume not being downloaded

--- src/comp/GoToLink.jsx
+++ src/comp/GoToLink.jsx
@@ -1,17 +1,17 @@
-
 const GoToLink = (url) => {
-    if(url === "phone"){
-        return null
-    }
-    else if(url === "resume"){
-        const link= document.createElement('a')
-        link.href = '../assets/Resume.pdf';
-        link.setAttribute('download', "Resume.pdf")
-        link.setAttribute('type', 'application/pdf')
+    if (url === "phone") {
+        return null;
+    } else if (url === "resume") {
+        const link = document.createElement('a');
+        link.href = '../assets/Resume.pdf'; // Ensure this path is correct
+        link.setAttribute('download', 'Resume.pdf');
+        link.setAttribute('type', 'application/pdf');
+        document.body.appendChild(link); // Append link to body
         link.click();
-    }else{
-        window.open(url, '_blank')
+        document.body.removeChild(link); // Clean up the link
+    } else {
+        window.open(url, '_blank');
     }
 }
 
-export default GoToLink
+export default GoToLink;
